5e87d2ee91cff0fa72469fe2766bad15c2b657c2
Kal <info@kalacademy.org>
2019-07-20 16:39:44 -0700

    React

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/Layout/Landing';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
